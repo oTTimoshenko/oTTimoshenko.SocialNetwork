bf7667312d389ca762b7452f488ed906701fabe8
Olexandr Timoshenko <OlexandrV.Timoshenko@livenau.net>
2018-12-07 00:16:00 +0200

    Creation of project architecture

---
+++ oTTimoshenko.SocialNetwork.DataAccess/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oTTimoshenko.SocialNetwork.DataAccess
+{
+    public class Class1
+    {
+    }
+}
